1b057f42f4e55912ef39567718435b497fb84f1d
Alec Rawlins <alec12999@yahoo.com>
2023-06-30 13:46:33 -0400

    api service to get country data

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getAllLocations() {
+    return this.http.get(`https://restcountries.com/v3.1/all`);
+  }
+  getSpecificLocation(location:string) {
+    return this.http.get(`https://restcountries.com/v3.1/name/${location}?fullText=true`);
+  }
+  searchLocation(location:string) {
+    return this.http.get(`https://restcountries.com/v3.1/name/${location}`);
+  }
+  searchBorderedCountry(code:string) {
+    return this.http.get(`https://restcountries.com/v3.1/alpha/${code}`)
+  }
+}
