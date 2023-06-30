b5f84297a635e6e14cd9dea1badae3617ffb15e1
Alec Rawlins <alec12999@yahoo.com>
2023-06-30 13:45:35 -0400

    initial commit for app comp, showing theme string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'countries-display';
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {
+    
+  }
+  theme: string = 'Dark Mode';
 }
