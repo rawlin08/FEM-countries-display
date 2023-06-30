3ee2a64943e22e3304d5c473f5681287f549024c
Alec Rawlins <alec12999@yahoo.com>
2023-06-30 18:52:20 -0400

    toggling theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-header',
   template: `
   <h1>Where in the world?</h1>
-  <button><svg class="icon"><use href="#moonIcon"></use></svg> Dark Mode</button>
+  <button (click)="toggleTheme()"><svg class="icon"><use href="#moonIcon"></use></svg> {{ this.app.theme.button }}</button>
   `,
   styles: [`
   h1 {
     font-size: 1em;
+    color: inherit;
   }
   button {
     background-color: transparent;
     border: none;
-    color: var(--iconColor);
+    color: inherit;
     font-size: 16px;
     padding: 10px;
     display: flex;
@@ -25,6 +27,25 @@ import { Component } from '@angular/core';
   }
   `]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(public app: AppComponent) {}
+  ngOnInit() {}
 
+  toggleTheme() {
+    if (this.app.theme.mode == 'light') {
+      this.app.theme = {
+        mode: 'dark',
+        button: 'Light Mode'
+      }
+      document.body.classList.replace('light', 'dark');
+    }
+    else {
+      this.app.theme = {
+        mode: 'light',
+        button: 'Dark Mode'
+      }
+      document.body.classList.replace('dark', 'light');
+    }
+    this.app.saveTheme();
+  }
 }
